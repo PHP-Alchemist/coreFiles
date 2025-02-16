1b6ef2aa9509e64098e61e45c9dd0daf1a6bf70d
Micah Breedlove <druid628@gmail.com>
2025-02-16 00:24:31 -0500

    Removes Magic Numbers

--- src/Traits/PerformanceTrait.php
+++ src/Traits/PerformanceTrait.php
@@ -4,27 +4,30 @@ namespace PHPAlchemist\Traits;
 
 trait PerformanceTrait
 {
+    const int ONE_KILOBYTE_IN_BYTES = 1024;
+    const int ONE_MEGABYTE_IN_BYTES = 1048576;
+
     /**
      * get the performance data (Peak Memory Usage) for a given script or
      * class.
      *
      * @return string
      */
-    public function getPerformance()
+    public function getPerformance() : string
     {
-        $mem_usage = memory_get_peak_usage();
-        if ($mem_usage < 1024) {
+        $memUsage = memory_get_peak_usage();
+        if ($memUsage < self::ONE_KILOBYTE_IN_BYTES) {
             // These two are ignored as the nature of phpUnit testing will not allow me to come in under a meg
             // and run coverage
             // @codeCoverageIgnoreStart
-            $whoa = $mem_usage.' bytes';
-        } elseif ($mem_usage < 1048576) {
-            $whoa = round($mem_usage / 1024, 2).' kilobytes';
+            $displayData = $memUsage . " bytes";
+        } elseif ($memUsage < self::ONE_MEGABYTE_IN_BYTES) {
+            $displayData = round($memUsage / self::ONE_KILOBYTE_IN_BYTES, 2) . " kilobytes";
             // @codeCoverageIgnoreEnd
         } else {
-            $whoa = round($mem_usage / 1048576, 2).' megabytes';
+            $displayData = round($memUsage / self::ONE_MEGABYTE_IN_BYTES, 2) . " megabytes";
         }
 
-        return $whoa;
+        return $displayData;
     }
 }
