59769f3a28d5c9079b03ee5bd4f3f76660ec8406
Micah Breedlove <druid628@gmail.com>
2025-05-29 16:12:20 -0400

    simplifies BadJsonException

--- src/Exceptions/BadJsonException.php
+++ src/Exceptions/BadJsonException.php
@@ -4,17 +4,5 @@ namespace PHPAlchemist\Exceptions;
 
 class BadJsonException extends \Exception
 {
-    const DEFAULT_MESSAGE = 'Invalid JSON';
-
-    public function __construct(
-        $message = self::DEFAULT_MESSAGE,
-        $code = 0,
-        $previous = null
-    ) {
-        if (empty($message)) {
-            $this->message = self::DEFAULT_MESSAGE;
-        }
-
-        parent::__construct($message, $code, $previous);
-    }
+    protected $message = 'Invalid JSON';
 }
